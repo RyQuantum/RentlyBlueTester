521d37e9754a6ca9b34368e8f431e2a35e7e9caa
Ryan <mayue3434@gmail.com>
2021-04-23 09:07:23 +0800

    update react-native-elements

--- app/screens/Settings/index.js
+++ app/screens/Settings/index.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
+import React, { Component, useState } from 'react';
+import { View, Text, Button, ScrollView } from 'react-native';
 import { connect } from 'react-redux';
-import { CheckBox } from 'react-native-elements';
+import { ButtonGroup, CheckBox, Icon, Input, ListItem } from 'react-native-elements';
 import Slider from '@react-native-community/slider';
 import { BarIndicator } from 'react-native-indicators';
 import RadioGroup from 'react-native-radio-button-group';
@@ -31,7 +31,7 @@ class Settings extends Component {
 
   render() {
     return (
-      <View>
+      <ScrollView>
         <View style={styles.settingsView}>
           <View style={{ opacity: this.props.checkEnabled ? 1: 0.5 }}>
             <CheckBox
@@ -82,10 +82,112 @@ class Settings extends Component {
                 <Text style={styles.serialNumber}>{this.props.maxSerialNumber}</Text>}
             </View>
           </View>
+          <ListItem
+            containerStyle={{}}
+            disabledStyle={{ opacity: 0.5 }}
+            onLongPress={() => console.log("onLongPress()")}
+            onPress={() => console.log("onPress()")}
+            pad={20}>
+            <ListItem.Content>
+              {/*<ListItem.Title>*/}
+              {/*  <Text>Pranshu Chittora</Text>*/}
+              {/*</ListItem.Title>*/}
+              <ListItem.Subtitle>
+                <Text>RSSI</Text>
+              </ListItem.Subtitle>
+              <Slider
+                step={1}
+                width="100%"
+                minimumValue={-100}
+                maximumValue={0}
+                value={this.state.initialRssiThreshold}
+                onValueChange={()=> {}}
+              />
+              <ListItem.Subtitle>
+                <Text>Battery</Text>
+              </ListItem.Subtitle>
+              <Slider
+                step={1}
+                width="100%"
+                minimumValue={-100}
+                maximumValue={0}
+                value={this.state.initialRssiThreshold}
+                onValueChange={()=> {}}
+              />
+              <Input
+                placeholder="Fob number"
+                leftIcon={{ type: 'font-awesome', name: 'credit-card' }}
+                style={styles}
+                onChangeText={value => this.setState({ comment: value })}
+              />
+            </ListItem.Content>
+          </ListItem>
+          <View style={{ height: 20 }}/>
+          <ListItem
+            containerStyle={{}}
+            disabledStyle={{ opacity: 0.5 }}
+            onLongPress={() => console.log("onLongPress()")}
+            onPress={() => console.log("onPress()")}
+            pad={20}>
+            <ListItem.Content>
+              <Text style={{ paddingBottom: 5 }}>Please get the current largest serial number</Text>
+              <ButtonGroup
+                onPress={() => {}}
+                selectedIndex={1}
+                buttons={['V3 Lock', 'Access Panel', '5G Lock']}
+                containerStyle={{}}
+              />
+              <Button title="Get Largest SN" />
+            </ListItem.Content>
+          </ListItem>
+          <View style={{ height: 20 }} />
+          <App />
         </View>
-      </View>
+      </ScrollView>
     );
   }
+}
+function App() {
+  const [expanded, setExpanded] = useState(false);
+  const list = [
+    {
+      name: 'Amy Farha',
+      avatar_url:
+        'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+      subtitle: 'Vice President',
+    }
+  ];
+  return (
+    <ListItem.Accordion
+      content={
+        <>
+          <Icon name="place" size={30} />
+          <ListItem.Content>
+            <ListItem.Title>RSSI filter</ListItem.Title>
+          </ListItem.Content>
+        </>
+      }
+      isExpanded={expanded}
+      onPress={() => {
+        setExpanded(!expanded);
+      }}>
+      {list.map((l, i) => (
+        <ListItem key={i} bottomDivider>
+          <ListItem.Subtitle>
+            <Text>RSSI</Text>
+          </ListItem.Subtitle>
+          <Slider
+            step={1}
+            width="80%"
+            minimumValue={-100}
+            maximumValue={0}
+            value={-50}
+            onValueChange={()=> {}}
+          />
+        </ListItem>
+      ))}
+    </ListItem.Accordion>
+  );
 }
 
 const mapStateToProps = (state) => state.locks;
